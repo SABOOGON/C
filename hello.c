4ef2e3aaaa8294cddb4b7441f48b96144e11bffc
SABOOGON <95422433+SABOOGON@users.noreply.github.com>
2021-12-09 17:07:12 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,77 @@
+int counter = 0;
+counter = counter + 1;
+counter++;
+
+
+
+if (x < y)
+{
+    printf("x is less than y\n");
+}
+
+
+
+if (x < y)
+[
+    printf("x is less than y\n");
+]
+else
+[
+    printf("x is not less than y\n");
+]
+
+
+
+if (x < y)
+{
+    printf("x is less than y\n");
+}
+else if (x > y)
+{
+    printf("x is greater than y\n");
+}
+else if (x == y)
+{
+    printf("x is equal to y\n");
+}
+
+
+
+while (true)
+{
+    printf("hello, world\n");
+}
+
+
+
+int i = 0;
+while (i < 50)
+{
+    printf("hello, world\n");
+    i++;
+}
+
+
+
+for (int i = 0; i < 50;, i++)
+{
+    printf("hello, world\n");
+}
+
+
+
+------------------------------------
+
+int i = 0;
+while (i < 10)
+{
+    printf("개발공부는 재미있다!\n");
+    i++;
+}
+
+
+
+for (int i = 0;, i < 10;, i++;)
+{
+    printf("개발공부는 재미있다!\n");
+}
