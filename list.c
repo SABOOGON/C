b3d048ceb206ea4928c4eba8f95f4ad66d838c5a
SABOOGON <95422433+SABOOGON@users.noreply.github.com>
2022-01-20 21:46:53 +0900

    Add files via upload

---
+++ list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *list = malloc(3 * sizeof(int));
+
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    int *tmp = malloc(4 * sizeof(int));
+
+    if (tmp == NULL)
+    {
+        return 1;
+    }
+
+    // 오래된 배열에서 새로운 배열로 정수를 복사하는 코드
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    for (int i= 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    free(list);
+}
